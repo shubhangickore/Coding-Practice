34f4ff78df819272dcc1725477c480f53f758fa9
shubhangickore <koreshubhangi291@gmail.com>
2026-04-07 23:46:47 +0530

    it return sum with small subarray

---
+++ MinSizeSubSum.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class MinSizeSubSum {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int target = sc.nextInt();
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(minsubarr(nums, target));
+
+    }
+
+    public static int minsubarr(int nums[], int target) {
+        int start = 0;
+        int sum = 0;
+        int minLen = Integer.MAX_VALUE;
+        int n = nums.length;
+
+        for (int end = 0; end < n; end++) {
+            sum += nums[end];
+
+            while (sum >= target) {
+                sum -= nums[start];
+                minLen = Math.min(minLen, end - start + 1);
+                start++;
+            }
+        }
+
+        return (minLen == Integer.MAX_VALUE) ? 0 : minLen;
+    }
+
+}
