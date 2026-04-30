ea07fdd51f0baa296189dd4ddc23783398e4e719
shubhangickore <koreshubhangi291@gmail.com>
2026-04-30 23:54:05 +0530

    It returns subarray with length k and it is nearer to the X

---
+++ KClosest.java
@@ -0,0 +1,22 @@
+public class KClosest {
+    public static List<Integer> findKclosest(int arr[], int k, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (right - left >= k) {
+            if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            result.add(arr[i]);
+        }
+
+        return result;
+    }
+
+}
