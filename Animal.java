3598fac98dc66c275e72a55ba7539f16b5a19006
shubhangickore <koreshubhangi291@gmail.com>
2026-06-05 23:55:44 +0530

    Implemented method overriding

---
+++ Animal.java
@@ -0,0 +1,20 @@
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public static void main(String[] args) {
+
+    Animal a = new Dog();
+    a.sound();
+}
